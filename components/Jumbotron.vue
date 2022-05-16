ca6cde799a1d3558238a4cf4088ee3fd21cfca17
Chavez Harris <codedbychavez@gmail.com>
2022-05-16 05:48:14 -0400

    Added sample app

---
+++ components/Jumbotron.vue
@@ -0,0 +1,23 @@
+<script>
+export default {}
+</script>
+
+<template>
+  <div>
+    <b-jumbotron class="jumbotron" header="AcmeTV Shows" lead="Get our monthly premium plan for just $5.99 when you sign up today">
+      <p>Access your favorite AcmeTV Shows on demand!</p>
+      <b-button variant="dark" href="#">
+        Sign up and start watching
+      </b-button>
+    </b-jumbotron>
+  </div>
+</template>
+
+<style>
+.jumbotron {
+  border-radius: 0px;
+  background-color: #F7CE68;
+  background-image: linear-gradient(62deg, #F7CE68 100%, #FBAB7E 0%);
+
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,28 @@
+<script>
+// Imports
+import Navigation from '../components/Navigation'
+import Jumbotron from '../components/Jumbotron'
+import TrendingMovies from '../components/TrendingMovies'
+import Movies from '../components/Movies'
+
+export default {
+  name: 'IndexPage',
+  components: {
+    Navigation,
+    Jumbotron,
+    TrendingMovies,
+    Movies
+  }
+
+}
+</script>
+
+<template>
+  <main>
+    <Navigation />
+    <Jumbotron />
+    <!-- Feature flag can toggle this -->
+    <TrendingMovies />
+    <Movies />
+  </main>
+</template>
